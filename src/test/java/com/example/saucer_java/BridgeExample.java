6ad3790558cdd04bd6dc88f71740e1b231f307e3
Ender <33337309+e3ndr@users.noreply.github.com>
2024-07-19 23:44:20 -0500

    Lol oops.

--- src/test/java/com/example/saucer_java/BridgeExample.java
+++ src/test/java/com/example/saucer_java/BridgeExample.java
@@ -1,10 +1,14 @@
-package com.example.webview_java;
+package com.example.saucer_java;
 
+import co.casterlabs.rakurai.json.element.JsonObject;
 import co.casterlabs.saucer.Saucer;
+import co.casterlabs.saucer.bridge.JavascriptFunction;
+import co.casterlabs.saucer.bridge.JavascriptGetter;
+import co.casterlabs.saucer.bridge.JavascriptObject;
 import co.casterlabs.saucer.utils.SaucerEmbeddedFiles;
 import co.casterlabs.saucer.utils.SaucerOptions;
 
-public class EmbeddingExample {
+public class BridgeExample {
 
     public static void main(String[] args) {
         Saucer saucer = Saucer.create(
@@ -23,12 +27,38 @@ public class EmbeddingExample {
                 "<!DOCTYPE html>"
                     + "<html>"
                     + "<h1>Hello world!</h1>"
+                    + "<button onclick='Example.openDevTools()'>Open Developer Tools</button><br />"
                     + "<sub>With ❤️ from saucer_java</sub>"
                     + "</html>"
             );
         saucer.webview().serve(files, "index.html"); // Tell Saucer to serve that file we made above.
 
+        saucer.bridge().defineConstant("thisIsAConstant", JsonObject.EMPTY_OBJECT);
+        saucer.bridge().defineObject("Example", new BridgeObjectExample(saucer));
+        saucer.bridge().apply();
+
         saucer.run(); // This blocks until the user closes the window.
+    }
+
+    @JavascriptObject
+    public static class BridgeObjectExample {
+        private Saucer saucer;
+
+        // Grab the saucer instance so we can open the devTools below.
+        public BridgeObjectExample(Saucer saucer) {
+            this.saucer = saucer;
+        }
+
+        @JavascriptGetter
+        public long nanoTime() {
+            return System.nanoTime();
+        }
+
+        @JavascriptFunction
+        public void openDevTools() {
+            this.saucer.webview().setDevtoolsVisible(true);
+        }
+
     }
 
 }
