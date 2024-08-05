7e3eba23ade92952851fa81a59015ab55aad8f33
e3ndr <33337309+e3ndr@users.noreply.github.com>
2024-08-05 08:44:33 -0500

    fix(examples): Use the new threading model.
    Also includes documentation on how that works now :D

--- examples/src/main/java/com/example/saucer_java/URLExample.java
+++ examples/src/main/java/com/example/saucer_java/URLExample.java
@@ -3,23 +3,30 @@ package com.example.saucer_java;
 import java.io.IOException;
 
 import co.casterlabs.saucer.Saucer;
+import co.casterlabs.saucer.Saucer.SaucerRunStrategy;
 import co.casterlabs.saucer.utils.SaucerOptions;
 
 public class URLExample {
 
     public static void main(String[] args) throws IOException {
-        Saucer saucer = Saucer.create(
-            new SaucerOptions()
-                .hardwareAcceleration(true) // May not work on all computers. You should do some testing to discover if you
-                                            // need this feature and if your environments support it.
-        );
+        // Queue the window for creation. You must call run() before a window can be
+        // created. dispatch() allows you to asynchronously wait for run() to be called.
+        Saucer.dispatch(() -> {
+            Saucer saucer = Saucer.create(
+                new SaucerOptions()
+                    .hardwareAcceleration(true) // May not work on all computers. You should do some testing to discover if you
+                                                // need this feature and if your environments support it.
+            );
 
-        saucer.webview().setContextMenuAllowed(true); // Allow the right-click menu.
+            saucer.webview().setContextMenuAllowed(true); // Allow the right-click menu.
 
-        saucer.webview().setUrl("https://duckduckgo.com");
+            saucer.webview().setUrl("https://duckduckgo.com");
 
-        saucer.window().show();
-        saucer.run(); // This blocks until the user closes the window.
+            saucer.window().show();
+        });
+
+        // This blocks until every window is closed.
+        Saucer.run(SaucerRunStrategy.RUN_UNTIL_ALL_CLOSED);
     }
 
 }
--- examples/src/main/java/com/example/saucer_java/UndecoratedExample.java
+++ examples/src/main/java/com/example/saucer_java/UndecoratedExample.java
@@ -3,26 +3,33 @@ package com.example.saucer_java;
 import java.io.IOException;
 
 import co.casterlabs.saucer.Saucer;
+import co.casterlabs.saucer.Saucer.SaucerRunStrategy;
 import co.casterlabs.saucer.scheme.SaucerSchemeHandler;
 import co.casterlabs.saucer.utils.SaucerOptions;
 
 public class UndecoratedExample {
 
     public static void main(String[] args) throws IOException {
-        Saucer saucer = Saucer.create(
-            new SaucerOptions()
-                .hardwareAcceleration(true) // May not work on all computers. You should do some testing to discover if you
-                                            // need this feature and if your environments support it.
-        );
+        // Queue the window for creation. You must call run() before a window can be
+        // created. dispatch() allows you to asynchronously wait for run() to be called.
+        Saucer.dispatch(() -> {
+            Saucer saucer = Saucer.create(
+                new SaucerOptions()
+                    .hardwareAcceleration(true) // May not work on all computers. You should do some testing to discover if you
+                                                // need this feature and if your environments support it.
+            );
 
-        saucer.window().showDecorations(false);
-        saucer.webview().setContextMenuAllowed(true); // Allow the right-click menu.
+            saucer.window().showDecorations(false);
+            saucer.webview().setContextMenuAllowed(true); // Allow the right-click menu.
 
-        saucer.webview().setSchemeHandler(SaucerSchemeHandler.fromResources(UndecoratedExample.class)); // Read the contents from our resources.
-        saucer.webview().serveScheme("UndecoratedExample.html");
+            saucer.webview().setSchemeHandler(SaucerSchemeHandler.fromResources(UndecoratedExample.class)); // Read the contents from our resources.
+            saucer.webview().serveScheme("UndecoratedExample.html");
 
-        saucer.window().show();
-        saucer.run(); // This blocks until the user closes the window.
+            saucer.window().show();
+        });
+
+        // This blocks until every window is closed.
+        Saucer.run(SaucerRunStrategy.RUN_UNTIL_ALL_CLOSED);
     }
 
 }
