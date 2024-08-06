796c5ffca94e73a74777aacfc0a3d68e031c787d
e3ndr <33337309+e3ndr@users.noreply.github.com>
2024-08-06 18:33:46 -0500

    feat(qt): add an experimental QT backend
    this supports aarch64 and mips64el but with some caveats and bugs.

---
+++ backends/qt_experimental/src/main/java/co/casterlabs/saucer/_impl/ExperimentalBackendQt5.java
@@ -0,0 +1,47 @@
+package co.casterlabs.saucer._impl;
+
+import java.io.IOException;
+
+import co.casterlabs.saucer._impl._SaucerBackend.FindThisSaucerBackend;
+
+@SuppressWarnings("deprecation")
+@FindThisSaucerBackend(-101)
+public class ExperimentalBackendQt5 extends _SaucerBackend {
+
+    @Override
+    public boolean checkDependencies() throws IOException {
+        return checkForLibraries(
+            "Qt5WebEngineCore",
+            "Qt5WebEngineWidgets",
+            "Qt5WebChannel",
+            "Qt5Widgets",
+            "Qt5Core"
+        );
+    }
+
+    @Override
+    public String[] supportedSystemTargets() {
+        return new String[] {
+                "GNU_Linux"
+        };
+    }
+
+    @Override
+    public String[] supportedArchTargets() {
+        return new String[] {
+                "aarch64",
+                "mips64el"
+        };
+    }
+
+    @Override
+    public String getType() {
+        return "Qt5";
+    }
+
+    @Override
+    public String getBuildType() {
+        return "Debug";
+    }
+
+}
