e4580575261f923be09ea2ccfe73885f1bb39436
e3ndr <33337309+e3ndr@users.noreply.github.com>
2024-10-22 10:52:13 -0500

    fix(backends): Forgot to register macOS.

--- api/src/main/java/co/casterlabs/saucer/Saucer.java
+++ api/src/main/java/co/casterlabs/saucer/Saucer.java
@@ -69,6 +69,9 @@ public interface Saucer extends Closeable {
             case WINDOWS_NT:
                 return "Windows";
 
+            case MACOS:
+                return "macOS";
+
             default:
                 return null;
         }
