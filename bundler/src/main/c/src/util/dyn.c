c485fd84dffe726e0cddbb23278370df9c0de7b7
e3ndr <33337309+e3ndr@users.noreply.github.com>
2024-12-09 17:40:10 -0600

    fix(bundler): Don't exit if DLL fails to load.
    This is a common non-issue :^)

--- bundler/src/main/c/src/util/dyn.c
+++ bundler/src/main/c/src/util/dyn.c
@@ -15,29 +15,33 @@ bool dyn_add_dir(char *path)
     LPWSTR widened_path = strings_widen(path);
     if (AddDllDirectory(widened_path) == 0)
     {
-        printf("Failed to add DLL directory '%s': %d.\n", path, GetLastError());
+        printf("Failed to add DLL directory '%s': %lu.\n", path, GetLastError());
         return false; // Early return.
     }
 
     // We have to preload all of the DLLs to avoid issues with AWT not being able to find them later.
     char *pattern = strings_concat(path, "\\*.dll");
 
-    WIN32_FIND_DATA finder_data;
+    WIN32_FIND_DATAA finder_data;
     HANDLE finder = FindFirstFileA(pattern, &finder_data);
     if (finder == INVALID_HANDLE_VALUE)
     {
-        printf("Failed to start search for: %ls\n", pattern);
+        printf("Failed to start search for: %s\n", pattern);
         return false;
     }
 
     do
     {
-        if (LoadLibraryExA(finder_data.cFileName, NULL, LOAD_LIBRARY_SEARCH_DEFAULT_DIRS) == NULL)
+        char *partial_path = finder_data.cFileName;
+        char *full_path = strings_concat(path, strings_concat("\\", partial_path)); // This is disgusting, idc.
+
+        if (LoadLibraryExA(full_path, NULL, LOAD_LIBRARY_SEARCH_DEFAULT_DIRS) == NULL &&
+            LoadLibraryExA(partial_path, NULL, LOAD_LIBRARY_SEARCH_DEFAULT_DIRS) == NULL)
         {
-            printf("Failed to load library '%s': %d\n", finder_data.cFileName, GetLastError());
-            return false;
+            printf("Failed to load library '%s': %lu, ignoring.\n", full_path, GetLastError());
+            // return false;
         }
-    } while (FindNextFile(finder, &finder_data));
+    } while (FindNextFileA(finder, &finder_data));
 
     FindClose(finder);
     return true;
