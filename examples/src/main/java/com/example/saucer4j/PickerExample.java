7cb90015a12c2814cf60ab8ee02275c9a60f80ea
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-12-29 16:10:16 -0600

    Examples: Refactor the filepicker example.

--- examples/src/main/java/com/example/saucer4j/PickerExample.java
+++ examples/src/main/java/com/example/saucer4j/PickerExample.java
@@ -11,35 +11,27 @@ public class PickerExample {
         SaucerApp.initialize("com.example.saucer4j", true);
 
         // Picking a save destination
-//        {
-//            File file = SaucerFilePicker.create()
-//                .filter("*.png", "*.jpg", "*.jpeg") // Only allow image files
-//                .save();
-//            System.out.println("Save destination picked: " + file);
-//        }
+//        File file = SaucerFilePicker.create()
+//            .filter("*.png", "*.jpg", "*.jpeg") // Only allow image files
+//            .save();
+//        System.out.println("Save destination picked: " + file);
 
         // Picking a single file
-        {
-            File file = SaucerFilePicker.create()
-                .filter("*.png", "*.jpg", "*.jpeg") // Only allow image files
-                .pickFile();
-            System.out.println("Single file picked: " + file);
-        }
+        File file = SaucerFilePicker.create()
+            .filter("*.png", "*.jpg", "*.jpeg") // Only allow image files
+            .pickFile();
+        System.out.println("Single file picked: " + file);
 
         // Picking a folder
-//        {
-//            File folder = SaucerFilePicker.create()
-//                .pickFolder();
-//            System.out.println("Folder picked: " + folder);
-//        }
+//        File folder = SaucerFilePicker.create()
+//            .pickFolder();
+//        System.out.println("Folder picked: " + folder);
 
         // Picking a multiple files
-//        {
-//            List<File> file = SaucerFilePicker.create()
-//                .filter("*.png", "*.jpg", "*.jpeg") // Only allow image files
-//                .pickFiles();
-//            System.out.println("Multiple files picked: " + file);
-//        }
+//        List<File> file = SaucerFilePicker.create()
+//            .filter("*.png", "*.jpg", "*.jpeg") // Only allow image files
+//            .pickFiles();
+//        System.out.println("Multiple files picked: " + file);
 
     }
 
